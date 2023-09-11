95558237c9a1f129e5ca0fd0aa54226915fbc3d5
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-09-11 21:03:36 +0300

    Add files via upload

--- Person.jsx
+++ Person.jsx
@@ -3,13 +3,16 @@ import './Person.css'
 
 function Person(props){
 
-    const {firstName, id} = props
+    const {firstName, id, email, img} = props
 
     return(
         
-         <div className='person-card'>
+
+        <div className='person-card'>
                 <h1>Name: {firstName}</h1>
+                <img src={img} />
                 <h2>ID: {id}</h2>
+                <h2>Email: {email}</h2>
         </div>
         
     )
